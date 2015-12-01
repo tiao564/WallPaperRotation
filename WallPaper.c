0d1f3a58a042060c2b578f071947cc292670622a
Tiao <Tiao@whatever.com>
2015-12-01 13:40:55 -0500

    added random wallpaper function

--- WallPaper.c
+++ WallPaper.c
@@ -14,26 +14,45 @@
 int main(int argc, char* argv[])
 {
 printf("Start\n");
-//int mode = argv[1];
+
+///////////////////////////////////////////////////////
+/*Get mode for program, rotating, random, controlled?*/
+///////////////////////////////////////////////////////
+if (argc<2)
+{
+  printf("Not enough arguments, exiting.\n");
+  exit(1);
+}
+int mode = atoi(argv[1]);
+
+//////////////////////////////
 /*Structures for directories*/
+//////////////////////////////
 struct dirent *pDirent;                     //Struct runs through directory
 DIR * pDir;                                 //Directory to open
 
+///////////////////////////////
 /*Strings for Directory paths*/
+///////////////////////////////
 char * homeDir = "/home/tiao/";             //Base to get to different files
 char * workingDir = "DarkBackgrounds/";     //Example file can change when finnished
 char finalDir[64];                          //Final Directory location to open
 
+/////////////////////////////////////
 /*File names and gnu command to run*/
+/////////////////////////////////////
 char * files[800];                           //List of files in folder that can be opened
 char file_name[100];                         //File name that will be opened
 char * base = "gsettings set org.gnome.desktop.background picture-uri file://";
 char base_and_path[100];
 
+/////////////////////////////////////
 /*Itterators and random number seed*/
+/////////////////////////////////////
 srand(time(NULL));                          //Seed for rand function
 int counter = 0;                            //Counter countes number of files in directory
 int i = 0;                                  //Iterrator for number generator
+int rand_num;
 finalDir[0] = '\0';                         //Need this to prevent garbage values
 file_name[0] = '\0';
 base_and_path[0] = '\0';
@@ -60,21 +79,42 @@ pDir = opendir(finalDir);                   //Open Dir
 
   strcat(base_and_path, base);
   strcat(base_and_path, finalDir);
-  while(i<counter)
+  
+  if(mode==1)
   {
-    printf("%s\n", base_and_path);
-    strcat(file_name, base_and_path);
-    strcat(file_name,"'");
-    strcat(file_name, files[i]);
-    strcat(file_name,"'");
-    printf("%s\n", file_name);
-    system(file_name);
-    
-    //printf("%d",mode);
-    file_name[0] = '\0';
-    sleep(1);
-    i++;
+    while(i<=counter)
+    {
+      printf("%s\n", base_and_path);
+      strcat(file_name, base_and_path);
+      strcat(file_name,"'");                //Quotes allow for files with spaces in their names
+      strcat(file_name, files[i]);
+      strcat(file_name,"'");
+      printf("%s\n", file_name);
+      //system(file_name);
+      
+      file_name[0] = '\0';
+      sleep(1);
+      i++;
+    }
   }
+
+  if(mode==2)
+  {
+    while(i<=counter)
+    {
+      rand_num = rand()%(counter+1);
+      strcat(file_name, base_and_path);
+      strcat(file_name,"'");                //Quotes allow for files with spaces in their names
+      strcat(file_name, files[rand_num]);
+      strcat(file_name,"'");
+      printf("%s\n", file_name);
+      system(file_name);
+      file_name[0]='\0';
+      sleep(1);
+      i++;
+    }
+  }
+
   closedir (pDir);
   return 0;
 }
