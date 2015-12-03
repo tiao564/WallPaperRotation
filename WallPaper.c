d8088d25008ba09c3e74a14b00f2297fe1bdb32a
Tiao <Tiao@whatever.com>
2015-12-03 13:53:34 -0500

    Added a lot of comments, changed where dir is closed to earlier on

--- WallPaper.c
+++ WallPaper.c
@@ -52,10 +52,7 @@ char finalDir[64];                          //Final Directory location to open
 /////////////////////////////////////
 /*File names and gnu command to run*/
 /////////////////////////////////////
-//char * files[800];                           //List of files in folder that can be opened
-//char file_name[100];                         //File name that will be opened
 char * base = "gsettings set org.gnome.desktop.background picture-uri file://";
-//char base_and_path[100];
 
 /////////////////////////////////////
 /*Itterators and random number seed*/
@@ -63,17 +60,22 @@ char * base = "gsettings set org.gnome.desktop.background picture-uri file://";
 srand(time(NULL));                          //Seed for rand function
 int counter = 0;                            //Counter countes number of files in directory
 int i = 0;                                  //Iterrator for number generator
-int rand_num;
 finalDir[0] = '\0';                         //Need this to prevent garbage values
 file_name[0] = '\0';
 base_and_path[0] = '\0';
 
-strcat(finalDir, homeDir);                  //Cat strings to get full Dir path
+////////////////////////////////////
+/*Cat strings to get full Dir path*/
+////////////////////////////////////
+strcat(finalDir, homeDir);                  
 strcat(finalDir, workingDir);
 
-printf("%s\n", finalDir);
 pDir = opendir(finalDir);                   //Open Dir
 
+//////////////////////////////////////////////////////
+/*Check to see if Dir is good, if so open it         /
+// and loop through grabbing all files not .. and . */
+//////////////////////////////////////////////////////
   if(pDir == NULL)
   {
     printf("Directory Could not be opened\n");
@@ -88,36 +90,61 @@ pDir = opendir(finalDir);                   //Open Dir
       }
   }
 
+closedir (pDir);                          //Return dir memeory
+
+//////////////////////////////////////////////////////////////
+/*Build the command to change wallpaper, only need file name*/
+//////////////////////////////////////////////////////////////
   strcat(base_and_path, base);
   strcat(base_and_path, finalDir);
   
+///////////////////////////////////////////////////////
+/*Change wall paper based on mode, 1 linear, 2 random*/
+///////////////////////////////////////////////////////
   changeWallpaper(counter, mode);
 
-  closedir (pDir);
   return 0;
 }
 
+///////////////////////////////////////////////////////////
+/*changeWallpaper(int count, int selected_mode)          */
+/*                                                       */
+/*Function selects a wallpaper, finishes building a      */
+/* command and then runs the command in the system       */
+/*Params:                                                */
+/* count: number of files in directory                   */
+/* Selected_mode: picks how files are selected           */
+///////////////////////////////////////////////////////////
 void changeWallpaper(int count, int selected_mode)
 {
   int i= 0;
-  while(i<count)
+  ///////////////////////////////////////////////////////////////
+  /*While you havent finished looping through files, keep going*/
+  ///////////////////////////////////////////////////////////////
+  while(1)
   {
+    ///////////////////////////////////////////////////////////////
+    /*Finish making the command by adding file name to a temp val*/
+    ///////////////////////////////////////////////////////////////
     printf("%s\n", base_and_path);
     strcat(file_name, base_and_path);
     strcat(file_name,"'");                //Quotes allow for files with spaces in their names
+    /////////////////////////////////////////
+    /*Like stated, 1 is linear, 2 is random*/
+    /////////////////////////////////////////
     if (selected_mode==1)
     {
-      strcat(file_name, files[i]);
+      strcat(file_name, files[(i%count)]);
     }
-    if(selected_mode==2)
+    else if(selected_mode==2)
     {
       int rand_num = rand()%(count+1); 
       strcat(file_name,files[rand_num]);
     }
     strcat(file_name,"'");
     printf("%s\n", file_name);
-    system(file_name);
-    file_name[0] = '\0';
+    system(file_name);                    //Run in terminal
+    file_name[0] = '\0';                  //Reset temp comman
     sleep(1);
     i++;
   } 
