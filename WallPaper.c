cb522beb4d559448e2e683c44752d636649aabf2
Tiao <Tiao@whatever.com>
2015-12-03 13:20:53 -0500

    Made changing wall papers into a function

--- WallPaper.c
+++ WallPaper.c
@@ -11,6 +11,17 @@
 #include <stdlib.h>
 #include <time.h>
 
+void changeWallpaper(int count, int selected_mode);
+
+/////////////////////////////////////
+/*File names and gnu command to run*/
+/////////////////////////////////////
+char * files[800];                           //List of files in folder that can be opened
+char file_name[100];                         //File name that will be opened
+char base_and_path[100];
+
+
+
 int main(int argc, char* argv[])
 {
 printf("Start\n");
@@ -41,10 +52,10 @@ char finalDir[64];                          //Final Directory location to open
 /////////////////////////////////////
 /*File names and gnu command to run*/
 /////////////////////////////////////
-char * files[800];                           //List of files in folder that can be opened
-char file_name[100];                         //File name that will be opened
+//char * files[800];                           //List of files in folder that can be opened
+//char file_name[100];                         //File name that will be opened
 char * base = "gsettings set org.gnome.desktop.background picture-uri file://";
-char base_and_path[100];
+//char base_and_path[100];
 
 /////////////////////////////////////
 /*Itterators and random number seed*/
@@ -80,41 +91,34 @@ pDir = opendir(finalDir);                   //Open Dir
   strcat(base_and_path, base);
   strcat(base_and_path, finalDir);
   
-  if(mode==1)
-  {
-    while(i<=counter)
-    {
-      printf("%s\n", base_and_path);
-      strcat(file_name, base_and_path);
-      strcat(file_name,"'");                //Quotes allow for files with spaces in their names
-      strcat(file_name, files[i]);
-      strcat(file_name,"'");
-      printf("%s\n", file_name);
-      //system(file_name);
-      
-      file_name[0] = '\0';
-      sleep(1);
-      i++;
-    }
-  }
-
-  if(mode==2)
-  {
-    while(i<=counter)
-    {
-      rand_num = rand()%(counter+1);
-      strcat(file_name, base_and_path);
-      strcat(file_name,"'");                //Quotes allow for files with spaces in their names
-      strcat(file_name, files[rand_num]);
-      strcat(file_name,"'");
-      printf("%s\n", file_name);
-      system(file_name);
-      file_name[0]='\0';
-      sleep(1);
-      i++;
-    }
-  }
+  changeWallpaper(counter, mode);
 
   closedir (pDir);
   return 0;
+}
+
+void changeWallpaper(int count, int selected_mode)
+{
+  int i= 0;
+  while(i<count)
+  {
+    printf("%s\n", base_and_path);
+    strcat(file_name, base_and_path);
+    strcat(file_name,"'");                //Quotes allow for files with spaces in their names
+    if (selected_mode==1)
+    {
+      strcat(file_name, files[i]);
+    }
+    if(selected_mode==2)
+    {
+      int rand_num = rand()%(count+1); 
+      strcat(file_name,files[rand_num]);
+    }
+    strcat(file_name,"'");
+    printf("%s\n", file_name);
+    system(file_name);
+    file_name[0] = '\0';
+    sleep(1);
+    i++;
+  } 
 }
